c89642a5380b5779995fe404a73f0052b7ffafef
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-10-26 17:33:45 +0300

    cOMMIT

--- resources/views/send_sms.blade.php
+++ resources/views/send_sms.blade.php
@@ -24,7 +24,6 @@
         <button type="submit" class="btn btn-primary" name="submit" value="single">Submit</button>
     </form>
 </div>
-
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
